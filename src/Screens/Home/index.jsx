7bbaf39d0370305c2cbddaca4748cd2a2e1ed0d8
juliafclima <juliafclima@hotmail.com>
2023-11-15 16:48:36 -0300

    feat: header e icons

--- src/Screens/Home/index.jsx
+++ src/Screens/Home/index.jsx
@@ -5,8 +5,8 @@ export function Home() {
   return (
     <View style={styles.container}>
       <View style={styles.header}>
-        <Text>Marvelgram</Text>
-        <View>
+        <Text>MARVELGRAM</Text>
+        <View style={styles.headerOptions}>
           <AntDesign name="hearto" size={24} color="black" />
           <Feather name="message-circle" size={24} color="black" />
         </View>
@@ -21,4 +21,18 @@ const styles = StyleSheet.create({
     backgroundColor: 'white',
     alignItems: 'center',
   },
+  header: {
+    paddingHorizontal: 10,
+    marginTop: 10,
+    height: 56,
+    width: '100%',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    flexDirection: 'row',
+  },
+  headerOptions: {
+    alignItems: 'center',
+    flexDirection: 'row',
+    gap: 10,
+  }
 });
