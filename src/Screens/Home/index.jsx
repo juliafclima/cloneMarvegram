a2117f3559d7a678d3622a1a7dbc552bb5b98965
juliafclima <juliafclima@hotmail.com>
2023-11-15 21:10:13 -0300

    postagem no feed

--- src/Screens/Home/index.jsx
+++ src/Screens/Home/index.jsx
@@ -1,6 +1,10 @@
-import { FlatList, Image, Text, View } from "react-native";
+import { FlatList, Image, ScrollView, Text, View } from "react-native";
+
 import { AntDesign, Feather } from '@expo/vector-icons';
+import { FontAwesome } from '@expo/vector-icons';
+
 import { styles } from '../Home/style';
+
 import storie1 from '../../Assets/baby-groot.png';
 import storie2 from '../../Assets/capita-marvel.png';
 import storie3 from '../../Assets/cosmo.png';
@@ -92,7 +96,7 @@ const DATA = [
     id: Math.random().toString(36).substring(2, 27),
     photoURL: storie16,
     nome: 'Viúva Negra',
-  }, 
+  },
 ];
 
 export function Home() {
@@ -105,23 +109,112 @@ export function Home() {
           <Feather name="message-circle" size={24} color="white" />
         </View>
       </View>
+      <ScrollView style={{ width: '100%' }}>
+        <View style={styles.stories}>
+          <FlatList
+            horizontal={true}
+            data={DATA}
+            keyExtractor={(item) => item.id}
+            renderItem={({ item }) => (
+              <View style={styles.storiesCard} key={item.id}>
+                <Image
+                  source={item.photoURL}
+                  style={styles.storiesCardImage}
+                />
+                <Text style={styles.storiesCardNome}>{item.nome}</Text>
+              </View>
+            )}
+          />
+        </View>
 
-      <View style={styles.stories}>
-        <FlatList
-          horizontal={true}
-          data={DATA}
-          keyExtractor={(item) => item.id}
-          renderItem={({ item }) => (
-            <View style={styles.storiesCard} key={item.id}>
+        <View style={styles.content}>
+          <View style={styles.contentHeader}>
+            <View style={styles.contentHeaderLeft}>
               <Image
-                source={item.photoURL}
-                style={styles.storiesCardImage}
+                style={styles.contentHeaderLeftImage}
+                source={{ uri: storie1 }}
               />
-              <Text style={styles.storiesCardNome}>{item.nome}</Text>
+              <Text style={styles.contentHeaderLeftText}>Baby Groot</Text>
             </View>
-          )}
-        />
-      </View>
+            <View style={styles.contentHeaderPoints}>
+              <FontAwesome name="circle" size={4} color="white" />
+              <FontAwesome name="circle" size={4} color="white" />
+              <FontAwesome name="circle" size={4} color="white" />
+            </View>
+          </View>
+          <View style={styles.contentImage}>
+            <Image
+              source={require('../../Assets/iamgroot.png')}
+              style={styles.contentImage}
+            />
+          </View>
+        </View>
+
+        <View style={styles.contentFooter}>
+          <View style={{ flexDirection: 'row', justifyContent: 'space-between', width: '100%' }}>
+            <View style={styles.contentFooterLeft}>
+              <AntDesign name="hearto" size={24} color="white" />
+              <AntDesign name="message1" size={24} color="white" />
+              <FontAwesome name="share" size={24} color="white" />
+            </View>
+            <Feather name="bookmark" size={24} color="white" />
+          </View>
+          <View style={{ gap: 8, marginTop: 12, paddingLeft: 6 }}>
+            <View style={{ flexDirection: 'row', gap: 6 }}>
+              <Text style={{ color: '#fff', fontWeight: 'bold' }}>Baby Groot</Text>
+              <Text style={{ color: '#fff' }}>I'M GROOT!</Text>
+            </View>
+            <Text style={{ color: '#fff' }}>Ver tradução</Text>
+            <Text style={{ color: '#fff' }}>Ver todos os 3 comentários</Text>
+            <Text style={{ color: '#fff' }}>Adicione um comentário...</Text>
+          </View>
+        </View>
+
+        {/* SEGUNDO POST */}
+
+        <View style={styles.content}>
+          <View style={styles.contentHeader}>
+            <View style={styles.contentHeaderLeft}>
+              <Image
+                style={styles.contentHeaderLeftImage}
+                source={{ uri: storie7 }}
+              />
+              <Text style={styles.contentHeaderLeftText}>Spider Man</Text>
+            </View>
+            <View style={styles.contentHeaderPoints}>
+              <FontAwesome name="circle" size={4} color="white" />
+              <FontAwesome name="circle" size={4} color="white" />
+              <FontAwesome name="circle" size={4} color="white" />
+            </View>
+          </View>
+          <View style={styles.contentImage}>
+            <Image
+              source={require('../../Assets/miranhamanos.png')}
+              style={styles.contentImage}
+            />
+          </View>
+        </View>
+
+        <View style={styles.contentFooter}>
+          <View style={{ flexDirection: 'row', justifyContent: 'space-between', width: '100%' }}>
+            <View style={styles.contentFooterLeft}>
+              <AntDesign name="hearto" size={24} color="white" />
+              <AntDesign name="message1" size={24} color="white" />
+              <FontAwesome name="share" size={24} color="white" />
+            </View>
+            <Feather name="bookmark" size={24} color="white" />
+          </View>
+          <View style={{ gap: 8, marginTop: 12, paddingLeft: 6 }}>
+            <View style={{flexDirection: 'row', gap: 6}}>
+              <Text style={{ color: '#fff', fontWeight: 'bold' }}>Spider Man</Text>
+              <Text style={{ color: '#fff' }}>Mais que miranhas, manos!</Text>
+            </View>
+            <Text style={{ color: '#fff' }}>Ver tradução</Text>
+            <Text style={{ color: '#fff' }}>Ver todos os 6 comentários</Text>
+            <Text style={{ color: '#fff' }}>Adicione um comentário...</Text>
+          </View>
+        </View>
+      </ScrollView>
     </View>
   );
 }
--- src/Screens/Home/style.jsx
+++ src/Screens/Home/style.jsx
@@ -9,7 +9,6 @@ export const styles = StyleSheet.create({
   header: {
     paddingHorizontal: 10,
     marginTop: 10,
-    height: 56,
     width: '95%',
     justifyContent: 'space-between',
     alignItems: 'center',
@@ -21,21 +20,22 @@ export const styles = StyleSheet.create({
     gap: 10,
   },
   stories: {
+    height: 114,
     width: '100%',
     paddingLeft: 10,
+    paddingVertical: 10,
     alignContent: 'center',
   },
   storiesCard: {
     marginRight: 14,
-    textAlign: 'center'
+    textAlign: 'center',
   },
   storiesCardImage: {
     borderWidth: 2,
-    padding: 2,
     borderColor: '#f7855a',
     width: 64,
     height: 64,
-    borderRadius: '50%'
+    borderRadius: 50,
   },
   storiesCardNome: {
     color: 'white',
@@ -43,5 +43,50 @@ export const styles = StyleSheet.create({
     fontSize: 12,
     marginTop: 3,
     fontWeight: 200,
-  }
+  },
+  content: {
+    width: '100%',
+    marginVertical: 15,
+  },
+  contentHeader: {
+    height: 52,
+    alignItems: 'center',
+    justifyContent: 'space-between',
+    flexDirection: 'row',
+    paddingHorizontal: 20,
+  },
+  contentHeaderPoints: {
+    flexDirection: 'row',
+    gap: 3,
+  },
+  contentHeaderLeft: {
+    height: "100%",
+    flexDirection: "row",
+    alignItems: "center",
+    gap: 10,
+  },
+  contentHeaderLeftImage: {
+    width: 30,
+    height: 30,
+    borderRadius: 50,
+    borderWidth: 2,
+    borderColor: "#fff",
+  },
+  contentHeaderLeftText: {
+    color: "#fff",
+  },
+  contentImage: {
+    width: "100%",
+    height: 300,
+  },
+  contentFooter: {
+    width: '100%',
+    paddingHorizontal: 10,
+    marginTop: 4,
+  },
+  contentFooterLeft: {
+    alignItems: 'center',
+    flexDirection: 'row',
+    gap: 10,
+  },
 });
